87e601a970b16324651ffa9ccb46d642b3ca870f
chenyihongyi <chenyihongyi@163.com>
2019-09-11 13:29:15 +0800

    提交代码

--- src/main/java/com/poi/excel/poi_excel/controller/ExcelController.java
+++ src/main/java/com/poi/excel/poi_excel/controller/ExcelController.java
@@ -1,24 +1,30 @@
 package com.poi.excel.poi_excel.controller;
 
+
 import com.poi.excel.poi_excel.entity.Product;
 import com.poi.excel.poi_excel.enums.StatusCode;
 import com.poi.excel.poi_excel.mapper.ProductMapper;
 import com.poi.excel.poi_excel.response.BaseResponse;
 import com.poi.excel.poi_excel.service.ProductService;
 import com.poi.excel.poi_excel.util.PoiUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
 import java.util.List;
 import java.util.Map;
 
@@ -78,6 +84,33 @@ public class ExcelController {
             log.debug("excel sheet写入数据发生异常:", e.getMessage());
         }
         return null;
+    }
+
+    /**
+     * 导入excel
+     * @param request
+     * @return
+     */
+    @RequestMapping(value = prefix+"/excel/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public BaseResponse uploadExcel(MultipartHttpServletRequest request){
+        BaseResponse response = new BaseResponse(StatusCode.Success);
+        try {
+        MultipartFile file = request.getFile("productFile");
+        if(file!=null){
+            String fileName = file.getOriginalFilename();
+            String suffix = StringUtils.substring(fileName, fileName.lastIndexOf(".") + 1);
+            //根据上传的excel文件构造workbook实例，注意区分xls与xlsx版本对应的实例
+            Workbook workbook = PoiUtil.getWorkbook(file, suffix);
+            //读取上传上来的excel的数据到list<Product>中
+        }
+            //插入数据到数据库
+
+        } catch (Exception e) {
+            log.error("上传excel导入数据 发生异常:", e.fillInStackTrace());
+            return new BaseResponse(StatusCode.Fail);
+        }
+        return response;
     }
 
 }
--- src/main/java/com/poi/excel/poi_excel/util/PoiUtil.java
+++ src/main/java/com/poi/excel/poi_excel/util/PoiUtil.java
@@ -1,11 +1,14 @@
 package com.poi.excel.poi_excel.util;
 
+import com.poi.excel.poi_excel.enums.WorkBookVersion;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
@@ -28,13 +31,14 @@ public class PoiUtil {
 
     /**
      * 下载excel
+     *
      * @param response
      * @param workbook
      * @param fileName
      * @throws Exception
      */
-    public static void downloadExcel(HttpServletResponse response, Workbook workbook, String fileName) throws Exception{
-        response.setHeader("Content-Disposition", "attachment;filename="+new String(fileName.getBytes("UTF-8"),"iso-8859-1"));
+    public static void downloadExcel(HttpServletResponse response, Workbook workbook, String fileName) throws Exception {
+        response.setHeader("Content-Disposition", "attachment;filename=" + new String(fileName.getBytes("UTF-8"), "iso-8859-1"));
         response.setContentType("application/ynd.ms-excel;charset=UTF-8");
         OutputStream out = response.getOutputStream();
         workbook.write(out);
@@ -42,20 +46,23 @@ public class PoiUtil {
         out.close();
     }
 
+
+
     /**
      * 填充数据到excel的sheet中
+     *
      * @param dataList
      * @param headers
      * @param sheetName
      * @return
      */
     public static Workbook fillExcelSheetData(List<Map<Integer, Object>> dataList, String[] headers, String sheetName) {
-      Workbook workbook = new HSSFWorkbook();
-      Sheet sheet = workbook.createSheet(sheetName);
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet(sheetName);
 
-      //创建sheet的第一行数据,即excel的表头
+        //创建sheet的第一行数据,即excel的表头
         Row headerRow = sheet.createRow(0);
-        for(int i=0; i<headers.length; i++){
+        for (int i = 0; i < headers.length; i++) {
             headerRow.createCell(i).setCellValue(headers[i]);
         }
 
@@ -64,30 +71,76 @@ public class PoiUtil {
         Row row;
         Object obj;
 
-        for(Map<Integer, Object> rowMap:dataList){
-            try{
+        for (Map<Integer, Object> rowMap : dataList) {
+            try {
                 row = sheet.createRow(rowIndex++);
 
                 //遍历表头行每一个key->取到实际的value
-                for(int i=0; i<headers.length; i++){
+                for (int i = 0; i < headers.length; i++) {
                     obj = rowMap.get(i);
 
-                    if(obj==null){
-                      row.createCell(i).setCellValue("");
-                    }else if(obj instanceof Date){
-                        String tempDate = simpleDateFormat.format((Date)obj);
-                        row.createCell(i).setCellValue(tempDate==null?"":tempDate);
-                    }else{
+                    if (obj == null) {
+                        row.createCell(i).setCellValue("");
+                    } else if (obj instanceof Date) {
+                        String tempDate = simpleDateFormat.format((Date) obj);
+                        row.createCell(i).setCellValue(tempDate == null ? "" : tempDate);
+                    } else {
                         row.createCell(i).setCellValue(String.valueOf(obj));
                     }
                 }
-            } catch (Exception e){
+            } catch (Exception e) {
                 log.debug("excel sheet写入数据发生异常:", e.fillInStackTrace());
             }
         }
-      return workbook;
+        return workbook;
     }
 
+    /**
+     * 分sheet导出
+     *
+     * @param dataList
+     * @param headers
+     * @param sheetName
+     * @return
+     */
+    public Workbook manageSheet(List<Map<Integer, Object>> dataList, String[] headers, String sheetName) {
+        final Integer sheetSize = 5;
+        int dataTotal = dataList.size();
 
+        int sheetTotal = (dataTotal % sheetSize == 0) ? dataTotal / sheetSize : (dataTotal / sheetSize + 1);
+        int start = 0;
+        int end = sheetSize;
+
+        List<Map<Integer, Object>> subList;
+        Workbook workbook = new HSSFWorkbook();
+        for (int i = 0; i < sheetTotal; i++) {
+            subList = dataList.subList(start, end);
+            workbook = this.fillExcelSheetData(subList, headers, sheetName + "_" + (i + 1) + workbook);
+            start += sheetSize;
+            end += sheetSize;
+            if (end >= dataTotal) {
+                end = dataTotal;
+            }
+        }
+        return workbook;
+    }
+
+    /**
+     * 根据file与后缀名区分获取workbook实例
+     *
+     * @param file
+     * @param suffix
+     * @return
+     * @throws Exception
+     */
+    public static Workbook getWorkbook(MultipartFile file, String suffix) throws Exception {
+        Workbook workbook = null;
+        if (WorkBookVersion.WorkBook2003Xls.getCode().equalsIgnoreCase(suffix)) {
+            workbook = new HSSFWorkbook(file.getInputStream());
+        } else if (WorkBookVersion.WorkBook2007Xlsx.getCode().equalsIgnoreCase(suffix)) {
+            workbook = new XSSFWorkbook(file.getInputStream());
+        }
+        return workbook;
+    }
 
 }
